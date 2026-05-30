37522bac31498a88e2087d9623d0ebec9d403860
AzuritilDev <mryeet30333@gmail.com>
2026-05-30 22:34:07 +0300

    Addition: Added the Quran display command to the list of commands in the output of the help command.

--- cogs/help.py
+++ cogs/help.py
@@ -14,6 +14,7 @@ class Help(commands.Cog):
         )
         # available commands
         embed.add_field(name="/help", value="(Lists all the available slash commands of the bot.).", inline=False)
+        embed.add_field(name="/quran", value="(Displays a Qur'ân verse based on the user's input.).", inline=False)
         await interaction.response.send_message(embed=embed, ephemeral=hide_response)
 
 async def setup(bot : commands.Bot) -> None:
