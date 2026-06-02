2ae994dec9952a9d11c7f1164d71d699524bf02a
AzuritilDev <mryeet30333@gmail.com>
2026-06-02 22:12:18 +0300

    Minor Alterifications: Re-adjusted the wording of the description of the third command.

--- cogs/help.py
+++ cogs/help.py
@@ -15,7 +15,7 @@ class Help(commands.Cog):
         # available commands
         embed.add_field(name="/help", value="(Lists all the available slash commands of the bot.).", inline=False)
         embed.add_field(name="/quran", value="(Displays a Qur'ân verse based on the user's input.).", inline=False)
-        embed.add_field(name="/set-daily-quran", value="(Displays a randomly selected verse on a set channel daily.).", inline=False)
+        embed.add_field(name="/set-daily-quran", value="(Displays verses on a selected channel daily.).", inline=False)
         await interaction.response.send_message(embed=embed, ephemeral=hide_response)
 
 async def setup(bot : commands.Bot) -> None:
