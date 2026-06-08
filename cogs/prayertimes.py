aa2122f4789156af33db77a5f75e92a9f3852fe0
AzuritilDev <mryeet30333@gmail.com>
2026-06-08 19:14:35 +0300

    Fix: Used the .name of CalculationMethod and Madhab objects instead of .value

--- cogs/prayertimes.py
+++ cogs/prayertimes.py
@@ -99,7 +99,7 @@ class PrayerTime(commands.Cog):
             embed.add_field(name="Asr", value=asr, inline=times_inline)
             embed.add_field(name="Maghrib", value=maghrib, inline=times_inline)
             embed.add_field(name="Isha", value=isha, inline=times_inline)
-            embed.set_footer(text=f"{countdown_text}\n\nCalculation Method: {method.value}\nAsr Method: {madhab.value}", icon_url=self.bot.user.avatar.url)
+            embed.set_footer(text=f"{countdown_text}\n\nCalculation Method: {method.name}\nAsr Method: {madhab.name}", icon_url=self.bot.user.avatar.url)
 
             await interaction.followup.send(embed=embed, ephemeral=hide_response)
         except Exception as e:
