2ef74736dbf15beb880f429765658326cbbcf0cf
AzuritilDev <mryeet30333@gmail.com>
2026-06-24 16:01:41 +0300

    Additional quotes.

--- utils/custom_states.py
+++ utils/custom_states.py
@@ -1,4 +1,8 @@
 quotes = ["""The ˹true˺ believers are only those whose 
 hearts tremble at the remembrance of Allah, 
 whose faith increases when His revelations are recited to them, 
-and who put their trust in their Lord."""]
+and who put their trust in their Lord. — 8:2""",
+"""Surely with ˹that˺ hardship comes ˹more˺ ease. — 94:6""",
+"""Allah does not require of any soul more than what it can afford. — 2:286""",
+"""Do not lose hope in Allah's mercy, for Allah certainly forgives all sins. — 39:53""",
+"""If you are grateful, I will certainly give you more. But if you are ungrateful, surely My punishment is severe. — 14:7"""]
--- utils/quotables.py
+++ utils/quotables.py
@@ -1,6 +1,10 @@
 # I don't wanna math.random() the entire Qur'ân database because verses may look out of context
+# CTRL + F references while adding references to avoid duplicate verse references
 quotable_verses = ["57:4", "13:28", "2:157", 
                    "94:6", "2:286", "3:54", 
                    "8:30", "14:7", "2:195", 
                    "94:8", "2:42", "4:78", 
-                   "6:73"]
+                   "6:73", "8:2", "39:53"]
+
+# To be 100% there aren't any duplicates
+set(quotable_verses)
