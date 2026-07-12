ace35b338b5a3fcdb262e680ed5a4ff7f979cb08
AzuritilDev <mryeet30333@gmail.com>
2026-07-12 23:17:47 +0300

    Import availableTimeFormats for future enchancement.

--- utils/beautify.py
+++ utils/beautify.py
@@ -1,7 +1,9 @@
-'''
-EXAMPLE:
-beautifyCalculationMethodClassName("MOON_SIGHTING_COMMITTEE") # returns "Moon Sighting Committee"
-beautifyCalculationMethodClassName("HANAFI") # returns "Hanafi"
-'''
+from geography import availableTimeFormats
+
 def beautifyCalculationMethodClassName(name : str):
+    '''
+    EXAMPLE:
+    beautifyCalculationMethodClassName("MOON_SIGHTING_COMMITTEE") # returns "Moon Sighting Committee"
+    beautifyCalculationMethodClassName("HANAFI") # returns "Hanafi"
+    '''
     return name.replace('_', ' ').title()
